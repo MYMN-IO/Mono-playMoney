71906ea80dac73ba2d27adf9a27fc4c8d4bd328a
MYMN-IO <code-it@mymn.io>
2018-02-25 19:24:38 +0100

    Initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers, The MonopolyMoney developers
+//
+// This file is part of MonopolyMoney.
+//
+// MonopolyMoney is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MonopolyMoney is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with MonopolyMoney.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0xf0, 0x04, 0xaf, 0xa5, 0xca, 0x7d, 0x42, 0x46, 0x9a, 0xbc, 0xff, 0x82, 0x00, 0x1a, 0x52, 0xa1  } };
+
+}
+
